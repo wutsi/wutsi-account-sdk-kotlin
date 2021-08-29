423c4bc8647050212526bf46effcafb92652da76
htchepannou <htchepannou@expedia.com>
2021-08-29 09:47:33 -0400

    update dependencies

--- src/main/kotlin/com/wutsi/platform/account/dto/CreateAccountRequest.kt
+++ src/main/kotlin/com/wutsi/platform/account/dto/CreateAccountRequest.kt
@@ -3,5 +3,6 @@ package com.wutsi.platform.account.dto
 import kotlin.String
 
 public data class CreateAccountRequest(
-  public val phoneNumber: String = ""
+  public val phoneNumber: String = "",
+  public val language: String = "en"
 )
