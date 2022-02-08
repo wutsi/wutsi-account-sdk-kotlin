88e5773f385a6dd4bb61bbf8f61642a56365cf92
wutsi <herve.tchepannou@wutsi.com>
2022-02-08 14:54:57 -0500

    make phone optional

--- src/main/kotlin/com/wutsi/platform/account/dto/PaymentMethodSummary.kt
+++ src/main/kotlin/com/wutsi/platform/account/dto/PaymentMethodSummary.kt
@@ -9,7 +9,7 @@ public data class PaymentMethodSummary(
   public val provider: String = "",
   public val ownerName: String = "",
   public val maskedNumber: String = "",
-  public val phone: Phone = Phone(),
+  public val phone: Phone? = null,
   public val created: OffsetDateTime = OffsetDateTime.now(),
   public val updated: OffsetDateTime = OffsetDateTime.now()
 )
